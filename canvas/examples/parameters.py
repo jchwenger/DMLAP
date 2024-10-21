ab0f2f3b944e36e93db484f11f86e9eeec03adab
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-10-21 21:32:41 +0100

    canvas | examples, format

--- canvas/examples/parameters.py
+++ canvas/examples/parameters.py
@@ -2,24 +2,31 @@ from py5canvas import *
 
 import numpy as np
 
+
 def parameters():
     # This will expose a 'params' variable
-    return {'Width': (100.0, {'min': 10, 'max': 200}),
-            'Height': (100.0, {'min': 10, 'max': 200}),
-            'Color': ([255, 0, 0], {'type':'color'}),
-            'Hue': (0.0, {'min': 0, 'max': 1}),
-            'Show' : True,
-            'Other': { # Unused parameters for demo purposes
-                'A text' : ('Hello World', {'multiline': True, 'buf_length': 2024}),
-                'Selection' : (0, {'selection': ['First option', 'Second option', 'Third option']}),
-                'Font Size': 30,
-                'A real number': 0.0,
-                'Int slider' : (0, {'min': 0, 'max': 10}),
-                }
-            }
+    return {
+        "Width": (100.0, {"min": 10, "max": 200}),
+        "Height": (100.0, {"min": 10, "max": 200}),
+        "Color": ([255, 0, 0], {"type": "color"}),
+        "Hue": (0.0, {"min": 0, "max": 1}),
+        "Show": True,
+        "Other": {  # Unused parameters for demo purposes
+            "A text": ("Hello World", {"multiline": True, "buf_length": 2024}),
+            "Selection": (
+                0,
+                {"selection": ["First option", "Second option", "Third option"]},
+            ),
+            "Font Size": 30,
+            "A real number": 0.0,
+            "Int slider": (0, {"min": 0, "max": 10}),
+        },
+    }
+
 
 def setup():
     create_canvas(512, 512)
+
 
 def draw():
     background(0)
@@ -27,23 +34,30 @@ def draw():
     # Once processed and by default, parameters are accessed with the labels
     # converted to lowercase and spaces replaced by underscores.
     # Center of screen
-    translate(width/2, height/2)
+    translate(width / 2, height / 2)
     if params.show:
         fill(255)
         text_size(params.other.font_size)
         text(params.other.a_text, [0, 0], center="True")
         fill(params.color)
-        rectangle(-params.width/2 + np.sin(frame_count/20)*200, -params.height/2, params.width, params.height)
+        rectangle(
+            -params.width / 2 + np.sin(frame_count / 20) * 200,
+            -params.height / 2,
+            params.width,
+            params.height,
+        )
     # Check if text changed. We use the dot notation,
     # as a string to check for a subparameter.
-    if 'other.a_text' in sketch.gui.changed:
-        print('Text changed')
-    if 'hue' in sketch.gui.changed:
+    if "other.a_text" in sketch.gui.changed:
+        print("Text changed")
+    if "hue" in sketch.gui.changed:
         sketch.set_gui_theme(params.hue)
-        print('Hue changed')
+        print("Hue changed")
+
 
 def key_pressed(key, modifier):
     if key == " ":
         toggle_gui()
+
 
 run()
