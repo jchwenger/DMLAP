252b0c75d1be85f6af72ef2c4e56039479034eda
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-10-21 18:27:06 +0100

    canvas | dcgan latent space

--- canvas/dcgan/dcgan_latent_space.py
+++ canvas/dcgan/dcgan_latent_space.py
@@ -1,23 +1,11 @@
 import os
+import urllib
 import pathlib
 
-import cv2
-import numpy as np
+import torch
 
 from py5canvas import *
 
-from numpy.linalg import norm
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-import torch
-from torch import nn
-import torch.nn.functional as F
-
-import torchvision as tv
-from torchvision.transforms import v2
-import torchvision.transforms.functional as TF
 
 # Get cpu, gpu or mps device for training
 device = (
@@ -27,21 +15,19 @@ device = (
     if torch.backends.mps.is_available()
     else "cpu"
 )
-device = "cpu" # bug with conv_transpose2d on mps
+device = "cpu"  # bug with conv_transpose2d on mps
 print(f"Using {device} device")
 
 # Output size
 w, h = 512, 512
+
 # Make sure this matches the latent dimension you trained on
 latent_dim = 100
 
-def random_latent_vector():
-    return torch.randn(1,100,1,1) * 1.0 # Try varying this multiplier
 
-# utils
-def denorm(x):
-    """Denormalize the outputs from [-1, 1] to [0,1] (generator with 'tanh' activation)"""
-    return (x * 0.5) + 0.5
+def random_latent_vector():
+    return torch.randn(1, 100, 1, 1) * 1.0  # Try varying this multiplier
+
 
 seed1 = random_latent_vector()
 seed2 = random_latent_vector()
@@ -52,42 +38,71 @@ b = seed1 @ seed2
 # Number of frames for interpolation (more, slower)
 n_frames = 100
 
+# Local path to your trained net
+# DCGAN_PATH  = pathlib.Path("../../python/models/dcgan_mnist/dcgan_mnist_g.iter_0936_scripted.pt")
+
+# If the script does not find the file, it will attempt downloading a model
+# (from my drive)
+# MNIST
+DCGAN_PATH = pathlib.Path("dcgan_mnist_g.iter_0936_scripted.pt")
+
+# FashionMNIST
+# DCGAN_PATH  = pathlib.Path("dcgan_fashion_mnist_g.iter_2239_scripted.pt")
+
+# Check if the model file exists, if not, download it
+if not DCGAN_PATH.exists():
+    # MNIST
+    url = "https://drive.usercontent.google.com/u/0/uc?id=1ZuePaVjXaAkQJTdeu060ELip6Ri6VMl2&export=download"
+    # FashionMNIST
+    # url = "https://drive.usercontent.google.com/u/0/uc?id=1ZOozan_vi6yCC6ulFtHHUS2jbT_BUUqd&export=download"
+    print()
+    print(f"Downloading model from {url}...")
+    urllib.request.urlretrieve(url, DCGAN_PATH)
+    print(f"Model downloaded and saved as {DCGAN_PATH}")
+
 # Load generator model
-DCGAN_PATH = pathlib.Path("../models/dcgan_fashion_mnist/dcgan_fashion_mnist_g.iter_2339_scripted.pt")
 G = torch.jit.load(DCGAN_PATH, map_location=device)
+
 print(G)
 print()
 print(f"Our model has {sum(p.numel() for p in G.parameters()):,} parameters.")
 
+
 def slerp(val, low, high):
-    # Compute the cosine of the angle between the vectors and clip 
+    # Compute the cosine of the angle between the vectors and clip
     # it to avoid out-of-bounds errors
-    omega = torch.acos(torch.clamp(low/torch.norm(low) @ high / torch.norm(high) -1.0, 1.0))
+    omega = torch.acos(
+        torch.clamp(low / torch.norm(low) @ high / torch.norm(high) - 1.0, 1.0)
+    )
     so = torch.sin(omega)
     return torch.where(
         so == 0,
         # If sin(omega) is 0, use LERP (linear interpolation)
         (1.0 - val) * low + val * high,
         # Otherwise perform spherical interpolation (SLERP)
-        (torch.sin((1.0 - val) * omega) / so) * low + (torch.sin(val * omega) / so) * high
+        (torch.sin((1.0 - val) * omega) / so) * low
+        + (torch.sin(val * omega) / so) * high,
     )
+
 
 # Runs the model on an input image
 def generate_image(model):
-    noise = slerp(((sketch.frame_count) % n_frames)/n_frames, a, b)
     with torch.no_grad():
-        generated_image = G(noise).detach().cpu().squeeze().numpy()
-    generated_image = np.clip(generated_image, -1, 1)
-    return generated_image*0.5 + 0.5
+        noise = slerp(((sketch.frame_count) % n_frames) / n_frames, a, b)
+        generated_image = G(noise).detach().cpu().squeeze()
+        generated_image = torch.clip(generated_image, -1, 1).numpy()
+    return generated_image * 0.5 + 0.5
+
 
 def setup():
     sketch.create_canvas(w, h)
     sketch.frame_rate(60)
 
+
 def draw():
     c = sketch.canvas
 
-    global a, b # We neeed this to modify a and b
+    global a, b  # We neeed this to modify a and b
 
     if sketch.frame_count % n_frames == 0:
         a, b = b, a
@@ -95,6 +110,7 @@ def draw():
 
     c.background(0)
     img = generate_image(G)
-    c.image(img, [0,0], [c.width, c.height], opacity=1)
+    c.image(img, [0, 0], [c.width, c.height], opacity=1)
+
 
 run()
