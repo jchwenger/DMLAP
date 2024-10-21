8026f3de52275719f2ea27049c74a7cb7f678e37
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-10-21 19:38:33 +0100

    canvas | pix2pix draw update

--- canvas/pix2pix/pix2pix_draw.py
+++ canvas/pix2pix/pix2pix_draw.py
@@ -1,11 +1,22 @@
-#!/usr/bin/env python3
+import os
+import pathlib
+
 import numpy as np
-from py5canvas import canvas
-from mlxtend.image import extract_face_landmarks
-import tensorflow as tf
-from tensorflow import keras
-import cv2
-reload(canvas)
+
+import torch
+from torchvision.transforms import v2
+
+from py5canvas import *
+
+# Get cpu, gpu or mps device for training
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
 # Create video input
 w, h = 512, 256
@@ -13,43 +24,80 @@ w, h = 512, 256
 # Empty result initially
 result = np.zeros((256, 256, 3))
 
-model_path = '../models/landmarks2rembrandt/e100_generator.hd5'
-
+PIX2PIX_PATH = (
+    "../../python/models/pix2pix_rembrandt/pix2pix_rembrandt.iter_10879_scripted.pt"
+)
 
 # Load pix2pix model
-generator = keras.models.load_model(model_path)
-print(generator.summary())
+G = torch.jit.load(PIX2PIX_PATH, map_location=device)
 
-# Runs the model on an input image
-def generate_image(model, img):
-    img = (img/255)*2.0 - 1.0 # Scale to [-1,1]
-    res = model(np.expand_dims(img, 0), training=True)[0].numpy()
-    return (res*0.5 + 0.5) # Scale to [0, 1] (Canvas is fine with that)
+print(G)
+print()
+print(f"Our model has {sum(p.numel() for p in G.parameters()):,} parameters.")
+
+
+def generate(model, image):
+    # from (h,w,c) to (c,h,w)
+    image = torch.permute(torch.tensor(image.copy()), (2, 0, 1))
+
+    image = v2.ToImage()(image)
+
+    # Check for grayscale (single channel) or with alpha (four) (with GPT 4o)
+    if image.shape[0] == 1:
+        # Convert grayscale to RGB by repeating the channel 3 times
+        image = image.repeat(3, 1, 1)
+    elif image.shape[0] == 4:
+        # Remove the alpha channel by taking the first 3 channels (RGB)
+        image = image[:3, :, :]
+
+    image = v2.Resize((256, 256), antialias=True)(image)
+    # from [0,255] to [0,1]
+    image = v2.ToDtype(torch.float32, scale=True)(image)
+    image = image.to(device)
+    # add a batch dimension
+    image = image[None, ...]
+    with torch.no_grad():
+        outputs = model(image).detach().cpu()
+    output = outputs[0].permute(1, 2, 0) * 0.5 + 0.5
+    return output.numpy()
+
+
+def brush(pos, delta):
+    size = np.linalg.norm(delta)
+    fill(255, 50)
+    no_stroke()
+    circle(pos, np.exp(-size * 0.03) * 10)
+
 
 def setup():
-    sketch.create_canvas(w, h)
-    sketch.frame_rate(20) # Our framerate will be pretty low anyhow
-    sketch.canvas.background(0)
+    create_canvas(w, h)
+    frame_rate(20)  # Our framerate will be pretty low anyhow
+    background(0)
+
 
 def draw():
-    c = sketch.canvas
+    stroke(255)
+    no_fill()
+    stroke_weight(1.0)
+    if dragging:
+        brush(mouse_pos, mouse_delta)
 
-    c.stroke(255)
-    c.no_fill()
-    c.stroke_weight(1.0)
-    if sketch.mouse_pressed:
-        c.line(sketch.mouse_pos - sketch.mouse_delta, sketch.mouse_pos)
+    image(result, [sketch.width / 2, 0], [sketch.width / 2, sketch.width / 2])
 
-    c.image(result, [sketch.width/2,0], [sketch.width/2, sketch.width/2])
 
-def key_pressed(k, modifier):
-    print('key pressed')
+def key_pressed(key, modifier):
+    print(f"key pressed: {key}")
+
+    # clear
+    if key == "c":
+        background(0)
+
+
+def mouse_released():
     global result
-    c = chr(k)
-    if c=='c':
-        sketch.canvas.background(0)
-    if c==' ':
-        print(sketch.canvas.width)
-        img = sketch.canvas.get_image()[:,:sketch.width//2,:]
-        img = cv2.resize(img, (256, 256))
-        result = generate_image(generator, img)
+    print("generating")
+    img = get_image()[:, : sketch.width // 2, :]
+    result = generate(G, img)
+
+
+run()
